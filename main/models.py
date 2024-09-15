108a9605cf23fac09c2312385e55a7de85eb9419
Attar <attaralwie@gmail.com>
2024-09-15 19:13:47 +0700

    Add input form for product

--- main/models.py
+++ main/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+import uuid
 
 class Product(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
-    # time = models.DateField(auto_now_add=True)
+    time = models.DateField(auto_now_add=True)
     price = models.IntegerField()
     description = models.TextField()
-    rating = models.FloatField()
     genre = models.TextField()
 
     # @property
